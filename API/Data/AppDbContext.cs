b95840a81b63a7bbc1fcb752b107eae652a0798b
a.ianniello <a.ianniello@betacom.it>
2026-03-11 17:39:11 +0100

    end of section 2

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Entities.AppUser> Users { get; set; }
+}
